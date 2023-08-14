b749c2c165d2796905e2cc67fd26cf3c471562f6
charitylefosa <charitylefosa@gmail.com>
2023-08-14 23:45:48 +0000

    tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    """ function that adds 2 tuples."""
+    tuple_a = tuple_a + (0, 0) if len(tuple_a) < 2 else tuple_a[:2]
+    tuple_b = tuple_b + (0, 0) if len(tuple_b) < 2 else tuple_b[:2]
+    result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return result
