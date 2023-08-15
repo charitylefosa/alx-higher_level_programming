749f134b18b24a47cf080e928bb48c08cfafa28a
charitylefosa <charitylefosa@gmail.com>
2023-08-15 00:13:06 +0000

    max not using max()

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    """ finds the biggest integer of a list."""
+    """ if my_list == []:
+        return None"""
+    value = my_list[0]
+    for num in my_list:
+        if num > value:
+            value = num
+    return value
