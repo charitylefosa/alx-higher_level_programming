9df15b4f1b8a0216061524b7948e212dd159f4ca
charitylefosa <charitylefosa@gmail.com>
2023-08-15 00:16:04 +0000

    included the empty condition

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     """ finds the biggest integer of a list."""
-    """ if my_list == []:
-        return None"""
+    if my_list == []:
+        return None
     value = my_list[0]
     for num in my_list:
         if num > value:
