3f44d1f84cb8d652f5cb96c25f26a2d7b7af8d98
charitylefosa <charitylefosa@gmail.com>
2023-08-16 20:24:42 +0000

    sorted keys

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    """ prints a dictionary by ordered keys. """
+    for keys in sorted(a_dictionary):
+        print("{:s}: {}".format(keys, a_dictionary[keys]))
