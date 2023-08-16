d49b129e0bb23066a3e42bdfda51c77ee90ca5a3
charitylefosa <charitylefosa@gmail.com>
2023-08-16 21:36:15 +0000

    indentation

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,8 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return None
-    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+    roman_numerals = {
+                      'I': 1, 'V': 5, 'X': 10, 'L': 50,
                       'C': 100, 'D': 500, 'M': 1000}
 
     prev_v = 0
