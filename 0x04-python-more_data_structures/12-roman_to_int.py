ac722314cae46e6ca658bc0a24e2ae84fcb777f4
charitylefosa <charitylefosa@gmail.com>
2023-08-16 21:38:48 +0000

    again

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,14 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return None
     roman_numerals = {
-                      'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                      'C': 100, 'D': 500, 'M': 1000}
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+                     }
 
     prev_v = 0
     output = 0
