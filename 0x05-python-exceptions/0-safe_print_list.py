82bfb2762d3e8c7d1c369b7704fdf6682d71fccc
charitylefosa <charitylefosa@gmail.com>
2023-08-28 22:47:38 +0000

    int list safe printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,10 +3,10 @@
 def safe_print_list(my_list=[], x=0):
     num_elements = 0
     for num_elements in range(x):
-       try:
-           print("{}".format(my_list[num_elements]), end="")
-           num_elements += 1
-       except Exception:
-           break
+        try:
+            print("{}".format(my_list[num_elements]), end="")
+            num_elements += 1
+        except Exception:
+            break
     print("")
     return (num_elements)
