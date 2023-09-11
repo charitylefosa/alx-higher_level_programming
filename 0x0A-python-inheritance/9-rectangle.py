76f2105c02543a86f7dd53a4f81a921a01287dac
charitylefosa <charitylefosa@gmail.com>
2023-09-11 23:47:33 +0000

    save again

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -19,4 +19,4 @@ class Rectangle(BaseGeometry):
     def __str__(self):
         """return string"""
         return("[{}] {}/{}".format(self.__class__.__name__,
-               self.__width, self.__height)).
+               self.__width, self.__height))
